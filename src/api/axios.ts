519843a8bfca6aadb158255d14fb3675f0768486
Sergey Ivanov <yeezusc322@gmail.com>
2023-08-02 16:30:23 +0300

    fix api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL ||'http://localhost:5001',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default instance;
